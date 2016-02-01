2d3c8c8bc02d64bbfe3dab2d22e159e4850b1d4e
song <icesong@outlook.com>
2016-02-01 11:50:24 +0800

    sorted

--- reduce_map.py
+++ reduce_map.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-
 from functools import reduce
+
 
 def f(s):  
     return int(s)
@@ -9,8 +9,7 @@ def f(s):
 def g(x, y):
     y /= pow(10, len(str(y)))
     return x + y
-    
-  
+
 l = reduce(g, map(f, '1234.45'.split('.')))
 print(l)
 
