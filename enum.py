be39a4114c4b6b7e6e2be1a057fe5183b488d67a
song <icesong@outlook.com>
2016-02-23 10:10:09 +0800

    枚举

---
+++ enum.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from enum import Enum
+
+# 枚举类
+# 定义枚举类
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+
+for name, member in Month.__members__.items():
+    print(name, '=>', member, ',', member.value)
+
+
+
